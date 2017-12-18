2fdd849f7559b3829ebf9e53835a0d9a0e2a95ec
Ran Randi <ran.r@techmarketing.co.il>
2017-12-18 18:41:54 +0200

    initial commit

---
+++ js/controllers.js
@@ -0,0 +1,50 @@
+var artistControllers = angular.module('artistControllers', ['ngAnimate']);
+
+artistControllers.controller('ListController',['$scope', '$http', function($scope, $http){
+    
+    $http.get('js/data.json').then(function(response){
+        
+        $scope.artists = response.data;
+        $scope.artistOrder = 'name';
+        
+    },function(error){
+        
+        console.log(error.statusText);
+    });
+    
+    
+}]);
+
+artistControllers.controller('DetailsController', ['$scope','$http', '$routeParams', function($scope, $http, $routeParams){
+    
+    
+    $http.get('js/data.json').then(function(response){
+        
+        $scope.artists =  response.data;
+        $scope.whichItem = $routeParams.itemId;
+        
+        if($routeParams.itemId > 0){
+            
+            $scope.prevItem = Number($routeParams.itemId) - 1 ;
+        
+        }else {
+            
+            $scope.prevItem = $scope.artists.length - 1;
+        }
+        
+        if($routeParams.itemId < $scope.artists.length - 1) {
+            
+            $scope.nextItem = Number($routeParams.itemId) + 1;
+        
+        }else {
+            
+            $scope.nextItem = 0;
+        }
+    }, function(error){
+        
+        console.log(error.statusText);
+    });
+    
+    
+    
+}]);
